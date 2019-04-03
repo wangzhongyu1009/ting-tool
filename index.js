bfd0affb1dcf43480e12880689794994e706af42
王忠玉 <wangzhongyu@epmworld.net>
2019-04-03 18:58:00 +0800

    '测试成功版本'

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var TING = function () {
+  var aaa = '999'
+}
+
+TING.prototype.getNowDateStr = function () {
+  return '123';
+}
+
+TING.prototype.getNowDateStr111 = function () {
+  return '222';
+}
+
+module.exports = new TING;
